ce5a56c3e9af31ae2cfc9d25c593a9efb48e2ec0
JozielOliveira <programdor.joziel@gmail.com>
2019-11-11 15:11:36 -0300

    replace addUser token

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken')
 
 const secretJWT = 'my-secret-from-env-file-in-prod'
 
-module.exports.addUser = ({ id, email, profession }) => jwt.sign({ id, email, profession }, secretJWT, { expiresIn: '30d' })
+module.exports.addUser = ({ id, email, profession }) => 
+  jwt.sign({ id, email, profession }, secretJWT, { expiresIn: '30d' })
 
 module.exports.getUser = token => {
   try {
